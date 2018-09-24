bc7fc60feb1cd969564668f565d37923678e66e8
Yuya <u4.yi3k8a@gmail.com>
2018-09-24 21:43:02 +0900

    Set up Dockercompose for local dev

--- dbmigrate/exec_migrate.sh
+++ dbmigrate/exec_migrate.sh
@@ -1,4 +1,1 @@
-#docker run -it    -e DOCKPGHOST=10.4.166.134    -e DOCKPGPORT=54320    -e DOCKPGDB=soylatte    -e DOCKPGUSER=postgres -e SECRET_KEY=hogehoge -e NEVERCACHE_KEY=hogehoge -e DOCKPGPASSWD=hogehoge -e DEBUG=True -p 8000:8000 test /usr/bin/python3 /project/soylatte/manage.py createdb
-#docker ps
-#docker run -it    -e DOCKPGHOST=10.4.166.134    -e DOCKPGPORT=54320    -e DOCKPGDB=soylatte    -e DOCKPGUSER=postgres -e SECRET_KEY=hogehoge -e NEVERCACHE_KEY=hogehoge -e DOCKPGPASSWD=hogehoge -e DEBUG=True -p 8000:8000 test /usr/bin/python3 /project/soylatte/manage.py makemigrations
-#docker run -it    -e DOCKPGHOST=10.4.166.134    -e DOCKPGPORT=54320    -e DOCKPGDB=soylatte    -e DOCKPGUSER=postgres -e SECRET_KEY=hogehoge -e NEVERCACHE_KEY=hogehoge -e DOCKPGPASSWD=hogehoge -e DEBUG=True -p 8000:8000 test /usr/bin/python3 /project/soylatte/manage.py migrate
+#docker run -it    -e DOCKPGHOST=10.4.162.151   -e DOCKPGPORT=54320    -e DOCKPGDB=soylatte    -e DOCKPGUSER=postgres -e SECRET_KEY=hogehoge -e NEVERCACHE_KEY=hogehoge -e DOCKPGPASSWD=Test1Pass -e DEBUG=True  soylatte_app:test /usr/bin/python3 /project/soylatte/manage.py migrate
--- nginx/docker-compose.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-export TEST1="mari"
-export TEST2="tetsuya"
-docker build . --build-arg TEST1 --build-arg TEST2
